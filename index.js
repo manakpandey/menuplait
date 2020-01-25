43fbbce8c8b148fb415ace9a3aedd27795719f64
manak <manakpandey@gmail.com>
2020-01-25 15:12:20 +0530

    Added ejs and database connection

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 const logger = require('./winston');
+const { dbUrl } = require('./config');
+const router = require('./routes/route');
 
 const app = express();
 const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', router);
+
+mongoose.connect(dbUrl, { useNewUrlParser: true }).catch((error) => {
+  logger.error(`Connection Error: ${error}`);
+});
 
 app.listen(port, () => logger.info(`App Listening on port ${port}`));
